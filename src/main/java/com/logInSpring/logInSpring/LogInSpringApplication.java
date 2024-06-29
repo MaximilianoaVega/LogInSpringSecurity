70c5b127cfc87a423b0c29341decc59f509914f2
maxi2594 <maxi2594@hotmail.com>
2024-06-29 17:11:40 -0300

    firt commit

---
+++ src/main/java/com/logInSpring/logInSpring/LogInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.logInSpring.logInSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogInSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logInSpring/logInSpring/LogInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logInSpring.logInSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
